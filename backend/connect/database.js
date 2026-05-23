1c96a1e56f556cf03564b455ae539a47abd9439e
dajana-dariu <dajanadariu21@gmail.com>
2026-05-23 12:10:55 +0200

    us

--- backend/connect/database.js
+++ backend/connect/database.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB connected:${connect.connection.host}`);
+    // Shtuam { family: 4 } si parametër të dytë këtu
+    const connect = await mongoose.connect(process.env.MONGO_URI, {
+      family: 4,
+    });
+    console.log(`MongoDB connected: ${connect.connection.host}`);
   } catch (err) {
     console.log(err);
     process.exit(1);
   }
 };
+
 module.exports = connectDB;
